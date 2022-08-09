5aaaba5affd09bece22b3cb763e5bff3d7cb9390
221B0825 <eunseoyu0825@gmail.com>
2022-08-09 23:55:40 +0900

    function

--- ch05.ts
+++ ch05.ts
@@ -16,3 +16,21 @@ const str = "hello world";
 //console.log((str as number[]).push(123)); //Error
 //문자열로 선언되있는 str 변수를 as number[] 를 통해 타입 에러를 무시하고 숫자형을 넣었을때
 //런타임 과정에서 에러가 발생한다.
+
+//함수 (Function)
+//함수의 인자
+//함수의 기본 타입 선언
+
+// //자바스크립트는 아래와 같이 선언한다
+// function sum(number1, number2){
+//     return number1 + number2;
+//     console.log(number1+2);
+// }
+function sum(number1: number, number2: number): number {
+  return number1 + number2;
+}
+console.log(sum(10, 20)); // result: 30
+
+function isChildren(age: number): boolean {
+  return age < 19;
+}
